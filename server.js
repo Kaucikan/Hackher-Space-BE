f9d1478a03816376605f5cec9833d441f63e6aac
Kaucikan G <gkaucikan595@gmail.com>
2026-04-09 17:39:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -198,8 +198,8 @@ mongoose
 
 /* -------------------- SERVER -------------------- */
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on", PORT);
 });
